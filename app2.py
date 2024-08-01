7893101f8658c98630d5534da87e29e52664a20c
charanvelugu <113339300+charanvelugu@users.noreply.github.com>
2024-08-01 10:48:09 +0530

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,76 @@
+### Health Management APP
+from dotenv import load_dotenv
+
+load_dotenv() ## load all the environment variables
+
+import streamlit as st
+import os
+import google.generativeai as genai
+from PIL import Image
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+## Function to load Google Gemini Pro Vision API And get response
+
+def get_gemini_repsonse(input,image,prompt):
+    model=genai.GenerativeModel('gemini-1.5-pro-latest')
+    response=model.generate_content([input,image[0],prompt])
+    return response.text
+
+def input_image_setup(uploaded_file):
+    # Check if a file has been uploaded
+    if uploaded_file is not None:
+        # Read the file into bytes
+        bytes_data = uploaded_file.getvalue()
+
+        image_parts = [
+            {
+                "mime_type": uploaded_file.type,  # Get the mime type of the uploaded file
+                "data": bytes_data
+            }
+        ]
+        return image_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
+    
+##initialize our streamlit app
+
+st.set_page_config(page_title="Gemini  App")
+
+input=st.text_input("Input Prompt: ",key="input")
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+image=""   
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
+
+
+submit=st.button(" Predict ")
+
+input_prompt="""
+
+
+            
+I am providing you with an image. Your task is to analyze the image and provide the following information about the person depicted:
+
+Gender: Identify if the person is male or female.
+Approximate Age: Estimate the person's age in years.
+Presence Confirmation: Confirm if there is indeed a person in the image.
+Please ensure your responses are based on the visual features present in the image. If the image is ambiguous or you cannot determine any of the details,
+ state that explicitly.
+
+Output Format:
+
+Gender: [Male/Female/Not Determinable]
+Age is : ---
+
+ 
+"""
+
+## If submit button is clicked
+
+if submit:
+    image_data=input_image_setup(uploaded_file)
+    response=get_gemini_repsonse(input_prompt,image_data,input)
+    st.subheader("The Response is")
+    st.write(response)
